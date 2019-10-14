f33f0b946c044ee8782a706eeee449bcd035cb9e
dewangga-pk <pacetcommunity570@gmail.com>
2019-10-14 15:42:50 +0700

    yey javascript

--- javascript.js
+++ javascript.js
@@ -1,6 +1,24 @@
-function myFunction() {
-    var x = document.getElementById("SideNav").style.height="160px";
+function displayNav() {
+  document.getElementById("SideNav").style.height="150px";
 }
 function closeNav() {
   document.getElementById("SideNav").style.height = "0";
+};
+
+function navClick() {
+  var x = document.getElementById("SideNav");
+  if (x.className == "Sidenavbar") {
+    x.className += " responsive";
+    displayNav();
+  } else {
+    x.className = "Sidenavbar";
+    closeNav();
+  }
 }
+
+let a1 = document.querySelectorAll(".jumbotron,.container,.testimoni,footer");
+for (let i =0 ,len = a1.length;i<len;i++){
+  a1[i].addEventListener('click',function(){
+    closeNav();
+  });
+};

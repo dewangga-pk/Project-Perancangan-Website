1d7591b298890908db92644c06b3d4e49540f0c0
dewangga-pk <pacetcommunity570@gmail.com>
2019-10-14 12:24:48 +0700

    weh click body bisa ngehide navnya

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("SideNav").style.height="200px";
+    var x = document.getElementById("SideNav").style.height="160px";
 }
 function closeNav() {
   document.getElementById("SideNav").style.height = "0";

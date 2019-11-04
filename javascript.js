832c6140d3d3852e7bfd50aa3da143a28caa5cf0
dewangga-pk <pacetcommunity570@gmail.com>
2019-11-04 20:28:54 +0700

    update web

--- javascript.js
+++ javascript.js
@@ -27,3 +27,18 @@ for (let i =0 ,len = a1.length;i<len;i++){
     closeNav();
   });
 };
+
+var modal = document.getElementById("myModal");                
+var btn = document.getElementById("myBtn");        
+var span = document.getElementsByClassName("close")[0];                 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
